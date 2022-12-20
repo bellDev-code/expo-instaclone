c08bfa608bac2dc271cc4daac5399e0dcd3eab99
bellDev-code <sumaoo20@naver.com>
2022-12-20 16:59:42 +0900

    Create AuthLayout & AuthButton & Add TextInput

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { TextInput } from "react-native";
 import styled from "styled-components/native";
+import AuthButton from "../components/auth/AuthButton";
+import AuthLayout from "../components/Auth/AuthLayout";
 
 const Container = styled.View`
   flex: 1;
@@ -9,8 +11,40 @@ const Container = styled.View`
 
 export default function CreateAccount() {
   return (
-    <Container>
-      <Text>CreateAccount</Text>
-    </Container>
+    <AuthLayout>
+      <TextInput
+        style={{ backgroundColor: "white", width: "100%" }}
+        placeholder="First Name"
+        returnKeyType="next"
+        placeholderTextColor="grey"
+      />
+      <TextInput
+        style={{ backgroundColor: "white", width: "100%" }}
+        placeholder="Last Name"
+        returnKeyType="next"
+        placeholderTextColor="grey"
+      />
+      <TextInput
+        style={{ backgroundColor: "white", width: "100%" }}
+        placeholder="UserName"
+        returnKeyType="next"
+        placeholderTextColor="grey"
+      />
+      <TextInput
+        style={{ backgroundColor: "white", width: "100%" }}
+        placeholder="Email"
+        keyboardType="email-address"
+        returnKeyType="next"
+        placeholderTextColor="grey"
+      />
+      <TextInput
+        style={{ backgroundColor: "white", width: "100%" }}
+        placeholder="Password"
+        secureTextEntry
+        returnKeyType="join"
+        placeholderTextColor="grey"
+      />
+      <AuthButton text="Create Account" disabled={true} onPress={() => null} />
+    </AuthLayout>
   );
 }
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,35 +2,8 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import { colors } from "../color";
-
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: black;
-  font-size: 22px;
-  padding: 0 40px;
-`;
-
-const Logo = styled.Image`
-  max-width: 50%;
-  height: 100px;
-`;
-
-const CreateAccount = styled.TouchableOpacity`
-  background-color: ${colors.blue};
-  padding: 13px 10px;
-  margin-top: 20px;
-  border-radius: 3px;
-  width: 100%;
-  opacity: ${(props) => (props.disabled ? "0.5" : "1")};
-`;
-
-const CreateAccountText = styled.Text`
-  color: ${colors.white};
-  font-weight: 600;
-  text-align: center;
-`;
+import AuthLayout from "../components/Auth/AuthLayout";
+import AuthButton from "../components/auth/AuthButton";
 
 const LoginLink = styled.Text`
   color: ${colors.blue};
@@ -42,14 +15,15 @@ export default function Welcome({ navigation }) {
   const goToCreateAccount = () => navigation.navigate("CreateAccount");
   const goToLogin = () => navigation.navigate("Login");
   return (
-    <Container>
-      <Logo resizeMode="contain" source={require("../assets/logo.png")} />
-      <CreateAccount disabled={false} onPress={goToCreateAccount}>
-        <CreateAccountText>Create Account</CreateAccountText>
-      </CreateAccount>
+    <AuthLayout>
+      <AuthButton
+        text="create New Account"
+        disabled={false}
+        onPress={goToCreateAccount}
+      />
       <TouchableOpacity onPress={goToLogin}>
         <LoginLink>Login</LoginLink>
       </TouchableOpacity>
-    </Container>
+    </AuthLayout>
   );
 }
